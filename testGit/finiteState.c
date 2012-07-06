5c69d796cfd7b9b8c1671703df3263ca1a73c7e8
Andrew Pierce <alp@alp-Ideapad-Z570.(none)>
2012-07-06 09:14:51 -0700

    cleaned up directory

--- testGit/finiteState.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int directMatch(const char* regex, const char* input, int regexSize, int inputSize){
-  for(ii = 0; ii < inputSize - regexSize; ii++){
-    if(strcmp(regex, input[]))
-  }
-}
